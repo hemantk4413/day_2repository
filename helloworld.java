2157d81df448fac1a623e2c2bc3ccd99a1c7f5e6
hemantk4413 <hemantkumbhar328@gmail.com>
2025-06-06 03:35:04 +0530

    Update helloworld.java
    small changes in message

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class HelloWorld 
 {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hemant here from Batch 39,nice to see you guys!");
     }
 }
